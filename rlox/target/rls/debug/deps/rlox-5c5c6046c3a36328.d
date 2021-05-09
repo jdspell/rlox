3be4419c259192faee1e335b8da272a558f91bc6
jdspell <spelljd15@gmail.com>
2021-05-08 21:42:45 -0400

    created chunk file which will define opcode types and functionality

---
+++ rlox/target/rls/debug/deps/rlox-5c5c6046c3a36328.d
@@ -0,0 +1,5 @@
+/home/jdpsell/Desktop/projects/rust/rlox/rlox/target/rls/debug/deps/rlox-5c5c6046c3a36328.rmeta: src/main.rs
+
+/home/jdpsell/Desktop/projects/rust/rlox/rlox/target/rls/debug/deps/rlox-5c5c6046c3a36328.d: src/main.rs
+
+src/main.rs:
---
+++ rlox/target/rls/debug/deps/rlox-e6e2528005ce01dd.d
@@ -0,0 +1,5 @@
+/home/jdpsell/Desktop/projects/rust/rlox/rlox/target/rls/debug/deps/rlox-e6e2528005ce01dd.rmeta: src/main.rs
+
+/home/jdpsell/Desktop/projects/rust/rlox/rlox/target/rls/debug/deps/rlox-e6e2528005ce01dd.d: src/main.rs
+
+src/main.rs:
